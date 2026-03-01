c410716e889a1923f269e27834c29afedcf31a17
Ryuu-spec <sshkor1102@gmail.com>
2026-03-01 21:41:38 +0900

    Update score.js

--- api/score.js
+++ api/score.js
@@ -1,3 +1,11 @@
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  },
+};
+
 export default async function handler(req, res) {
   // [핵심] 모든 출처(Origin) 및 Genially 도메인 허용 설정
   res.setHeader('Access-Control-Allow-Credentials', 'true');
